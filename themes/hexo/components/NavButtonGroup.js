8646bdf67263f138f9fda3a11e95b91e7fc3aec3
曾經、 <106513458+Ever-Abel@users.noreply.github.com>
2024-05-15 21:55:27 +0800

    Update NavButtonGroup.js

--- themes/hexo/components/NavButtonGroup.js
+++ themes/hexo/components/NavButtonGroup.js
@@ -12,7 +12,7 @@ const NavButtonGroup = (props) => {
   }
 
   return (
-    <nav id='home-nav-button' className={'w-full z-10 md:h-72 md:mt-6 xl:mt-32 px-5 py-2 mt-8 flex flex-wrap md:max-w-6xl space-y-2 md:space-y-0 md:flex justify-center max-h-80 overflow-auto'}>
+    <nav id='home-nav-button' className={'w-full z-10 md:h-96 md:mt-10 xl:mt-40 px-10 py-4 mt-10 flex flex-wrap md:max-w-7xl space-y-4 md:space-y-0 md:flex justify-center max-h-96 overflow-auto'}>
       {categoryOptions?.map(category => {
         return (
           <Link
@@ -20,7 +20,7 @@ const NavButtonGroup = (props) => {
             title={`${category.name}`}
             href={`/category/${category.name}`}
             passHref
-            className='text-center shadow-text w-full sm:w-4/5 md:mx-6 md:w-40 md:h-14 lg:h-20 h-14 justify-center items-center flex border-2 cursor-pointer rounded-lg glassmorphism hover:bg-white hover:text-black duration-200 hover:scale-105 transform'>
+            className='text-center shadow-text w-full sm:w-3/4 md:mx-8 md:w-48 md:h-20 lg:h-24 h-20 text-lg justify-center items-center flex border-2 cursor-pointer rounded-lg glassmorphism hover:bg-white hover:text-black duration-200 hover:scale-105 transform'>
                {category.name}
             </Link>
         )
